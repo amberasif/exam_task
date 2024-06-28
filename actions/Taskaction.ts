a15ca41902a5757eaf047eac693313c14f1a562b
CSLab Administrator <cslabadmin@shu.edu.pk>
2024-06-28 11:53:47 +0500

    all actions created

--- actions/Taskaction.ts
+++ actions/Taskaction.ts
@@ -19,3 +19,31 @@ export const CreateEntry = async (name: string, isCompleted: boolean) =>
     }
 };
 
+export const GetEntry = async () =>
+    {
+      try{
+        const entries = await
+        db.task.findMany();
+        return entries;
+    } catch (error){
+        console.error("error getting data from database",error);
+
+        throw new Error("error getting data from database");
+    }
+};
+export const DeleteEntry = async (id: string) =>
+    {
+      try{
+        const result = await
+        db.task.delete({
+            where:{
+                id,
+            },
+        })
+        return result;
+    } catch (error){
+        console.error("error deleting data from database",error);
+
+        throw new Error("error deleting data from database");
+    }
+};
